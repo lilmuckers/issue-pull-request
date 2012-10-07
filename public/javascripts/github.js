6821d53ed352407327ac4c8c9a3351cced011400
Patrick McKinley <patrick@patrick-mckinley.com>
2012-10-07 19:41:30 +0100

    Added loading overlays, pretty warnings, and some potential tooltips

--- public/javascripts/github.js
+++ public/javascripts/github.js
@@ -32,6 +32,9 @@ function GitHub(){
   //set up initial PR info
   this.resetPullInfo();
   
+  //overlay
+  this.overlay = $('.overlay');
+  
   //run the magic
   this.initialise();
 }
@@ -45,6 +48,20 @@ GitHub.prototype.initialise = function(){
   this.issues.select.bind('selection', $.proxy(this.issueSelect, this));
   
   this.finishButton.button.click($.proxy(this.submitData, this));
+  /*
+  this.formatTooltip(this.forkSelect, 'right');
+  this.formatTooltip(this.baseBranch.select, 'left');
+  this.formatTooltip(this.headBranch.select, 'right');
+  this.formatTooltip(this.issues.select, 'left');*/
+}
+
+GitHub.prototype.formatTooltip = function(button, position){
+  button.popover({
+    placement: position,
+    trigger: 'hover',
+    title: button.next().find('h3').html(),
+    content: button.next().find('p').html()
+  });
 }
 
 GitHub.prototype.baseBranchSelect = function(){
@@ -151,6 +168,7 @@ GitHub.prototype.populateDropdown = function(dropdown, data){
 
 GitHub.prototype.submitData = function(){
   if(this.isReadyToGo()){
+    this.showOverlay();
     $.post('/create-pull', this.pullRequestInformation, $.proxy(this.pullRequestResponse, this));
   }
   return false;
@@ -160,13 +178,31 @@ GitHub.prototype.pullRequestResponse = function(data, status, xhr){
   if(data.html_url){
     window.location.replace(data.html_url);
   } else {
-    alert('An unknown error occurred at github');
+    this.alert('An unknown error occurred at github', 'error');
+    this.hideOverlay();
   }
 }
 
 GitHub.prototype.forkSelectEvent = function(){
   //get the selected repo from the data
   var data = { repo: this.forkSelect.data('selected') };
+  
+  //bring up the overlay
+  this.showOverlay();
+  
+  //fadeOut the containers if needed
+  if(this.headBranch.containers.is(':visible')){
+    this.headBranch.containers.fadeOut();
+  }
+  if(this.baseBranch.containers.is(':visible')){
+    this.baseBranch.containers.fadeOut();
+  }
+  if(this.issues.containers.is(':visible')){
+    this.issues.containers.fadeOut();
+  }
+  if(this.finishButton.containers.is(':visible')){
+    this.finishButton.containers.fadeOut();
+  }
   
   //perform the ajax request
   $.post('/repo-select', data, $.proxy(this.formatForkSelectResponse, this));
@@ -187,19 +223,27 @@ GitHub.prototype.resetPullInfo = function(){
   }
 }
 
+GitHub.prototype.showOverlay = function(){
+  this.overlay.fadeIn(500);
+}
+GitHub.prototype.hideOverlay = function(){
+  this.overlay.fadeOut(500);
+}
+
 GitHub.prototype.formatForkSelectResponse = function(data, status, xhr){
   //reset the pull request information
   this.resetPullInfo();
   
   //make sure it all went well
   if(status != 'success'){
-    alert('Unknown error, please reload and try again');
+    this.hideOverlay();
+    this.alert('Unknown error, please reload and try again', error);
     return;
   }
   
   //build the head branch list and display it
   this.buildBranchList(data.head.branches, this.headBranch.select);
-  this.headBranch.containers.show();
+  this.headBranch.containers.fadeIn();
   
   //update the small link on the base branch list
   var link = this.baseBranch.containers.find('h3 small a');
@@ -208,7 +252,7 @@ GitHub.prototype.formatForkSelectResponse = function(data, status, xhr){
   
   //build the base branch list and display it
   this.buildBranchList(data.base.branches, this.baseBranch.select, true);
-  this.baseBranch.containers.show();
+  this.baseBranch.containers.fadeIn();
   
   //update the issue link
   var link = this.issues.containers.find('h3 small a');
@@ -216,10 +260,13 @@ GitHub.prototype.formatForkSelectResponse = function(data, status, xhr){
   
   //build the list of issues and display it
   this.buildIssueList(data.base.issues);
-  this.issues.containers.show();
+  this.issues.containers.fadeIn();
   
   //show the finish button
-  this.finishButton.containers.show();
+  this.finishButton.containers.fadeIn();
+  
+  //hide the overlay
+  this.hideOverlay();
   
   //now we need to set up the base information for the pull
   this.pullRequestInformation.user = data.head.repo.owner.login;
@@ -264,6 +311,24 @@ GitHub.prototype.buildBranchList = function(data, dropdown, master){
   }
   //run the population script
   this.populateDropdown(dropdown, branchDropdownData);
+}
+
+GitHub.prototype.alert = function(message, warn){
+  switch(warn){
+    case 'error':
+      var classname = 'alert-error';
+      var msg = 'Error!';
+      break;
+    case 'warning':
+    default:
+      var classname = '';
+      var msg = 'Warning!';
+      break;
+  }
+
+  var alert = $('<div class="alert hide '+classname+'"><button type="button" class="close" data-dismiss="alert">x</button><strong>'+msg+'</strong> '+message+'</div>');
+  this.alertsArea.append(alert);
+  alert.slideDown(100);
 }
 
 var github = new GitHub();
